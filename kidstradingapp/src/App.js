624845796c16c1f9822188c86e9ce559f363926c
Misha <misha06bender@gmail.com>
2019-03-06 11:35:16 -0800

    boiiiiiiiiiii

--- kidstradingapp/src/App.js
+++ kidstradingapp/src/App.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import imageOne from "./resources/Home.jpg";
 
 class App extends Component {
-  render() {
+  constructor(props){
+      super(props);
+      this.state={buttonState:0};
+    }
+
+    render() {
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        <button class="ui button" onClick={() => {
+          this.setState({buttonState:this.state.buttonState++});
+        }}>Get Started</button>
+          
+          {this.state.buttonState  >1 ?
+        (this.state.buttonState == 1 ? <img resource= "http://www3.pictures.livingly.com/gi/Misha+Bender+jU0rbv8NDyGm.jpg" /> : <img resource= "imageTwo" />)  
+         
+        
+        :
+        
+        <img src= "/Users/student/Desktop/Kid-s-Trading-App/kidstradingapp/src/resources/Home.jpg" />
+        
+        }
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           
           </a>
         </header>
       </div>
